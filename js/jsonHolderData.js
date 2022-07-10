b38a2656bc1b86d5aa49d3fd85bc16861676da3d
Javier Hector Araya <javier.araya.h@gmail.com>
2022-07-10 18:42:14 -0300

    agregandoFetch

---
+++ js/jsonHolderData.js
@@ -0,0 +1,24 @@
+
+for(i = 1; i <= 20; i++){
+    console.log(i)
+fetch(`https://jsonplaceholder.typicode.com/posts/${i}`)
+  .then((response) => response.json())
+  .then((json) => {
+  const div = document.createElement('div')
+  div.innerHTML = `
+
+    
+  <div class="testimonial-item bg-success text-white">
+  
+  <h3 class="bg-primary text-white">Cliente numero ${json.id}</h3>
+  <p>
+    "${json.body}"
+  </p>
+  
+</div>
+
+    
+    `
+    infoNose.append(div)
+})
+}
